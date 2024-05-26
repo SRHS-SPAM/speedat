6703a900e2f979279d883ed8b844252c26055ed5
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-05-26 22:59:08 +0900

    0526-4
    주석 추가

--- apps/client/components/ui/nav/footer.tsx
+++ apps/client/components/ui/nav/footer.tsx
@@ -2,15 +2,15 @@
 import { useState } from "react";
 import Image from "next/image";
 const Footer = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false); //다크모드 관리용
   return (
     <div className="w-full h-[80px] bg-white dark:bg-black">
       <div className=" relative flex flex-col">
         <div className=" justify-between w-[100px] flex absolute left-1/2 my-[3px] mx-[-50px]">
           <Image
-            src={isDarkMode ? "/githubDark.svg" : "/github.png"}
+            src={isDarkMode ? "/githubDark.svg" : "/github.png"} //isDarkMode에 따라서 이미지 변경
             alt="git"
-            className="cursor-pointer"
+            className="cursor-pointer" //마우스 올렸을 시 클릭 모양 만들기
             width={30}
             height={30}
             priority
--- apps/client/components/ui/nav/header.tsx
+++ apps/client/components/ui/nav/header.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useState } from "react";
-import { Bell, LogIn, Settings, User } from "lucide-react";
+import { Bell, LogIn, Settings, User } from "lucide-react"; //루시드-여러가지 모양의 아이콘 지원하는 라이브러리
 import Image from "next/image";
 const Header = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [isLogIn, setIsLogIn] = useState(false);
-  const iconColor = isDarkMode ? "#FFF" : "#000";
+  const [isDarkMode, setIsDarkMode] = useState(false); //다크모드 관리용
+  const [isLogIn, setIsLogIn] = useState(false); //로그인 여부 확인용
+  const iconColor = isDarkMode ? "#FFF" : "#000"; //다크모드에 따른 배경색 변경용
   return (
     <>
       <div className="fixed top-0 w-full min-w-[433px] h-[79px] px-[35px] py-2 dark:bg-black bg-white justify-between items-center inline-flex z-[9999]">
@@ -18,7 +18,7 @@ const Header = () => {
           priority
         />
         <div className="justify-start items-start gap-12 flex">
-          {isLogIn ? (
+          {isLogIn ? ( //로그인 여부에 따른 아이콘 이미지 변경용
             <User color={iconColor} className="w-9 h-9 relative cursor-pointer"></User>
           ) : (
             <LogIn color={iconColor} className="white w-9 h-9 relative cursor-pointer" />
