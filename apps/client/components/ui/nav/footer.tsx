0eb40b4614ca652b1dd49aa8da3f735dadcb65bc
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-05-26 21:33:05 +0900

    0526-2
    footer이미지 수정

--- apps/client/components/ui/nav/footer.tsx
+++ apps/client/components/ui/nav/footer.tsx
@@ -7,31 +7,23 @@ const Footer = () => {
     <div className="w-full h-[80px] bg-white dark:bg-black">
       <div className=" relative flex flex-col">
         <div className=" justify-between w-[100px] flex absolute left-1/2 my-[3px] mx-[-50px]">
-          {isDarkMode ? (
-            <Image
-              src="/githubDark.svg"
-              alt="git"
-              className="cursor-pointer"
-              width={30}
-              height={30}
-              priority
-            />
-          ) : (
-            <Image src="/github.png" alt="git" width={30} height={30} className="cursor-pointer" />
-          )}
+          <Image
+            src={isDarkMode ? "/githubDark.svg" : "/github.png"}
+            alt="git"
+            className="cursor-pointer"
+            width={30}
+            height={30}
+            priority
+          />
           <div className="text-black mt-[5px] dark:text-white">|</div>
-          {isDarkMode ? (
-            <Image
-              src="/instaDark.svg"
-              alt="insta"
-              className="cursor-pointer"
-              width={30}
-              height={30}
-              priority
-            />
-          ) : (
-            <Image src="/insta.png" alt="insta" width={30} height={30} className="cursor-pointer" />
-          )}
+          <Image
+            src={isDarkMode ? "/instaDark.svg" : "/insta.png"}
+            alt="insta"
+            className="cursor-pointer"
+            width={30}
+            height={30}
+            priority
+          />
         </div>
         <div className="text-black w-[1000px] text-center font-normal line absolute left-1/2 my-[40px] mx-[-500px] dark:text-white">
           rogo.wiki | msg2324@srh-spam.com | SPAM | PROJECT ROGOWIKI{" "}
