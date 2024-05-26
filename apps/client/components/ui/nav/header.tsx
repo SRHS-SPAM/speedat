42929db07df360c05c8c0feb394ba896ddd6c9e2
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-05-26 22:36:25 +0900

    0526-3
    컬러 변경 코드 변경

--- apps/client/components/ui/nav/header.tsx
+++ apps/client/components/ui/nav/header.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 const Header = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isLogIn, setIsLogIn] = useState(false);
+  const iconColor = isDarkMode ? "#FFF" : "#000";
   return (
     <>
       <div className="fixed top-0 w-full min-w-[433px] h-[79px] px-[35px] py-2 dark:bg-black bg-white justify-between items-center inline-flex z-[9999]">
@@ -18,21 +19,12 @@ const Header = () => {
         />
         <div className="justify-start items-start gap-12 flex">
           {isLogIn ? (
-            <User
-              color={isDarkMode ? "#FFF" : "#000"}
-              className="w-9 h-9 relative cursor-pointer"></User>
+            <User color={iconColor} className="w-9 h-9 relative cursor-pointer"></User>
           ) : (
-            <LogIn
-              color={isDarkMode ? "#FFF" : "#000"}
-              className="white w-9 h-9 relative cursor-pointer"
-            />
+            <LogIn color={iconColor} className="white w-9 h-9 relative cursor-pointer" />
           )}
-          <Bell
-            color={isDarkMode ? "#FFF" : "#000"}
-            className="w-9 h-9 relative cursor-pointer"></Bell>
-          <Settings
-            color={isDarkMode ? "#FFF" : "#000"}
-            className="w-9 h-9 relative cursor-pointer"></Settings>
+          <Bell color={iconColor} className="w-9 h-9 relative cursor-pointer"></Bell>
+          <Settings color={iconColor} className="w-9 h-9 relative cursor-pointer"></Settings>
         </div>
       </div>
     </>
