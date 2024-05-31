5b226fa7430821a6896d200254025cf35865ffa0
msg2324 <igiza1213@gmail.com>
2024-05-31 12:25:19 +0900

    아직 storybook 미완성

--- apps/client/app/(root)/layout.tsx
+++ apps/client/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/nav/header";
-import Footer from "@/components/nav/footer";
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
 
 export default function RootLayout({
   children,
