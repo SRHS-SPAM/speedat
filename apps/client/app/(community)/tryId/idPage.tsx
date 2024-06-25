aa455f99e32d2138a206f19b1e43679a569bd545
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-06-25 20:42:06 +0900

    스토리 및 상세페이지 시작

---
+++ apps/client/app/(community)/tryId/idPage.tsx
@@ -0,0 +1,11 @@
+const inId = {
+  title: "사회적 기업의 가치와 영향력",
+  name: "커피중독자",
+  tab: "시스템과 게시판",
+  best: "1",
+  time: "10",
+};
+const IdPage = () => {
+  return <></>;
+};
+export default IdPage;
