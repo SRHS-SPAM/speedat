927811857d022f27a8821fdba666ff0af059cbac
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-06-26 16:26:08 +0900

    상세페이지 작업중

---
+++ apps/client/app/(community)/community/[id]/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className="flex flex-row justify-start">{children}</div>
+    </>
+  );
+}
